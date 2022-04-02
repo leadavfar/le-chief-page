20c3dec8ad722a332c7e33f507a65adc39150304
Leandro Farias <leandrodavidfarias@gmail.com>
2022-04-02 20:44:31 -0300

    done

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "./logo.png"
+import logoWhite from "../../public/logo - white.png"
 import { Link } from "react-router-dom";
 import Styles from './NavBar.module.css';
 
@@ -6,12 +8,20 @@ export default function NavBar() {
     return (
         <div className={Styles.container}>
             <nav className={Styles.navbar}>
-                <div><Link to="/">Le Chief Page</Link></div>
-                <ul>
+                <Link to="/">
+                    <div className={Styles.logo}>
+                        <img src={logoWhite} width='30px' height='30px' alt="not found" />
+                        <h1>Le Chief Page</h1>
+                    </div>
+                </Link>
+                {/* <ul>
                     <li> <Link to="/recipes">Recipes</Link></li>
-                    {/* <li> <Link to="/recipes">Recipes</Link></li> */}
-                    <li><Link to= "/crud">Manage Recipes</Link></li>
-                </ul>
+                    <li><Link to="/crud">Manage Recipes</Link></li>
+                </ul> */}
+                <div className={Styles.links}>
+                    <h3><Link to="/recipes">Recipes</Link></h3>
+                    <h3><Link to="/crud">Manage Recipes</Link></h3>
+                </div>
             </nav>
         </div>
     )
