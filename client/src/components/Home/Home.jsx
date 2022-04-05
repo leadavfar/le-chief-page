268dde626bbc1754b08a84661050e8d3857fabd5
Leandro Farias <leandrodavidfarias@gmail.com>
2022-04-05 03:18:51 -0300

    done

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import image from '../../public/kirsty2.jpg'
+import Styles from './Home.module.css'
 
 export default function Home() {
     return (
@@ -7,8 +9,9 @@ export default function Home() {
             <div>
                 <NavBar />
             </div>
-            <div>
-                <h1>LE CHIEF PAGE</h1>
+            <div className={Styles.container}>
+                {/* <h1>LE CHIEF PAGE</h1> */}
+                <img src={image} height='600px' alt='image not found' />
             </div>
         </div>
     )
