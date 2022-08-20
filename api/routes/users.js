c09fe77c938d8b8b71472b78b7e24880bd93cace
Leandro Farias <leandrodavidfarias@gmail.com>
2022-08-20 03:18:24 -0300

    done

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', function (req, res) {
-    res.send("<h1>Aca se vienen los usuarios perrito malvado<h1>")
+    res.send("<h1>Users route<h1>")
 });
 
 module.exports = router;
